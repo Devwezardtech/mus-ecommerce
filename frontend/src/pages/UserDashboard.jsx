149ee1c90ec5cf5e81fcd568e1160b7353e14e17
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-09-14 16:51:01 +0800

    responsive and nice UI in user role

--- frontend/src/pages/UserDashboard.jsx
+++ frontend/src/pages/UserDashboard.jsx
@@ -83,123 +83,114 @@ const showMessage = (msg, type) => {
       <div className="fixed w-full z-50" >
         <HeaderUser/>
       </div>
-    <div className="p-4 bg-gray-100">
-    <div>
-      {products.length === 0 ? (
-        
-         <div className="px-4 mt-16 sm:px-2 md:px-8 lg:px-16 py-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-    {Array.from({ length: 8 }).map((_, index) => (
-      <div key={index} className="bg-gray-200 p-4 rounded-lg shadow-sm animate-pulse space-y-4">
-        <div className="h-48 bg-gray-300 rounded"></div>
-        <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-        <div className="h-4 bg-gray-300 rounded w-1/2"></div>
-        <div className="flex justify-between mt-2">
-          <div className="h-8 w-20 bg-gray-300 rounded"></div>
-          <div className="h-8 w-20 bg-gray-300 rounded"></div>
-        </div>
-      </div>
-    ))}
-    
- </div>
 
-      ) : (
-        <div>
-        <div className="px-1 mt-16 sm:px-2 md:px-8 lg:px-16
-         py-1 sm:py-2 md:py-4 lg:py-8
-         grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {products.map((product) => (
-            <div key={product._id}>
-              
-                <div className="bg-gray-250 p-2 flex flex-col rounded shadow-md hover:shadow-lg transition-shadow duration-300 gap-2 w-70 lg:w-60">
-                  <button onClick={() => setModalProduct(product)}>
-                <div className="flex flex-col mb-2">
-                <img 
+
+   <div className="p-4 bg-gray-100">
+        {products.length === 0 ? (
+          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mt-5 gap-4 py-8 lg:mt-16">
+            {Array.from({ length: 32 }).map((_, index) => (
+              <div
+                key={index}
+                className="bg-gray-200 rounded shadow animate-pulse p-1 space-y-1 lg:space-y-4 lg:p-4"
+              >
+                <div className="h-28 bg-gray-300 rounded lg:h-48"></div>
+                <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+                <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+                <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+                <div className="flex justify-between mx-1 gap-1 lg:mt-2 ">
+                  <div className="h-5 w-20 bg-gray-300 lg:h-8 rounded"></div>
+                  <div className="h-5 w-20  w-20 bg-gray-300 lg:h-8 rounded"></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="pt-10 sm:pt-14 md:pt-16 lg:pt-18">
+
+            <div className="grid gap-4 px-1 py-4 grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+              {products.map((product) => (
+                <div key={product._id}>
+                  <div className="bg-gray-250 mb-4 pb-2 flex flex-col rounded shadow-md hover:shadow-lg transition-shadow duration-300 gap-2 w-auto h-auto">
+                    <button onClick={() => setModalProduct(product)}>
+                      <div className="flex flex-col items-center justify-center">
+                        <img
                src={product.photo}
                alt={product.name}
-                className="h-60 w-60 object-cover rounded shadow"
-              />
-              <div className="p-2" >
-                 <strong className="text-gray-600">{product.name}</strong>
-              <p>{product.description}</p>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-               <p className="text-sm text-gray-600">Stock: {product.stock}</p>
+                          className="h-28 w-28 object-cover rounded shadow sm:h-32 sm:w-32 md:h-40 md:w-40 lg:h-60 lg:w-60 "/>
+                        <div>
+                          <strong className="text-gray-800 text-sm font-semibold sm:text-base md:text-md lg:text-lg">{product.name}</strong>
+                          <p className="text-xs">{product.description}</p>
+                          <p>
+                            <strong className="text-sm font-semibold sm:text-base md:text-md lg:text-lg">${product.price}</strong>
+                          </p>
+                          <p className="text-sx text-sm text-gray-500 sm:text-sm md:text-sm lg:text-md">
+                            Stock: {product.stock}
+                          </p>
+                        </div>
+                      </div>
+                    </button>
 
-              </div>
-              
-             
-              </div>
-              </button>
-              
-              
-              <div className="flex items-center justify-center gap-2">
-                <button onClick={() => handleAddToCart(product._id)} 
-              className=" p-1 m-1 bg-gray-400 text-white rounded hover:bg-gray-300 hover:text-black shadow-lg hover:shadow-md transition-shadow duration-200"
-              > ADD TO CART
-              </button>{" "}
-              <button onClick={() => handleBuy(product)}
-                className=" p-1 m-1 bg-gray-400 text-white rounded hover:bg-gray-300 hover:text-black shadow-lg hover:shadow-md transition-shadow duration-200"
-                >BUY</button>
-              
-              </div>
-              
-              </div>
-              
-              
-              
-              
+                    <div className="flex items-center justify-center gap-1">
+                      <button
+                        onClick={() => handleAddToCart(product._id)}
+                        className="p-1 bg-gray-400 text-white text-sm rounded hover:bg-gray-300 hover:text-black sm:text-base md:text-base lg:text-md"
+                      >
+                         <span className="block sm:hidden md:hidden">CART</span>
+                         <span className="hidden sm:block">ADD TO CART</span>
+                      </button>
+                      <button
+                        onClick={() => handleBuy(product)}
+                        className="p-1 bg-gray-400 text-white text-sm rounded hover:bg-gray-300 hover:text-black sm:text-base md:text-base lg:text-md"
+                      >
+                        BUY
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+        )}
       </div>
-       {/**
-      <AffiliateProducts />
-      */} 
-        
+
+      {modalProduct && (
+        <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50">
+          <div className="bg-gray-300 rounded-lg p-4 shadow-lg w-full max-w-sm relative">
+            <button
+              onClick={() => setModalProduct(null)}
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-200 text-xl bg-gray-200 hover:bg-gray-400 rounded w-10"
+            >
+              ✕
+            </button>
+            <img
+        src={modalProduct.photo}
+        alt={modalProduct.name}
+              className="w-full h-74 object-cover rounded-lg mb-4 py-8"
+            />
+            <h2 className="text-xl font-bold text-gray-700 mb-2">
+              {modalProduct.name}
+            </h2>
+            <p className="text-gray-600">{modalProduct.description}</p>
+            <p className="text-gray-800 font-bold mt-2">${modalProduct.price}</p>
+            <div className="flex justify-end gap-2 mt-4">
+              <button
+                onClick={() => handleAddToCart(modalProduct._id)}
+                className="bg-gray-500 text-white px-3 py-1 rounded-lg hover:bg-gray-400 hover:text-black"
+              >
+                Add to Cart
+              </button>
+              <button
+                onClick={() => handleBuy(modalProduct)}
+                className="bg-gray-500 text-white px-3 py-1 rounded-lg hover:bg-gray-400 hover:text-black"
+              >
+                Buy
+              </button>
+            </div>
+          </div>
         </div>
       )}
-    </div>
-
-    </div>
-
-    {modalProduct && (
-  <div className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center z-50">
-    <div className="bg-gray-300 rounded-lg p-4 shadow-lg w-full max-w-sm relative">
-      <button
-        onClick={() => setModalProduct(null)}
-        className="absolute w-10 top-2 right-2 text-gray-500 hover:text-gray-200 text-xl bg-gray-200 hover:bg-gray-400 rounded "
-      >
-        ✕
-      </button>
-      <img
-      src={modalProduct.photo}
-        alt={modalProduct.name}
-        className="w-full h-74 object-cover rounded-lg mb-4 py-8"
-      />
-      <h2 className="text-xl font-bold text-gray-700 mb-2">{modalProduct.name}</h2>
-      <p className="text-gray-600">{modalProduct.description}</p>
-      <p className="text-gray-800 font-bold mt-2">${modalProduct.price}</p>
-      <div className="flex justify-end gap-2 mt-4">
-        <button
-          onClick={() => handleAddToCart(modalProduct._id)}
-          className="bg-gray-500 text-white px-3 py-1 rounded-lg hover:bg-gray-400 hover:text-black"
-        >
-          Add to Cart
-        </button>
-        <button
-          onClick={() => handleBuy(modalProduct)}
-          className="bg-gray-500 text-white px-3 py-1 rounded-lg hover:bg-gray-400 hover:text-black"
-        >
-          Buy
-        </button>
-      </div>
-    </div>
-  </div>
-)}
 
 
-      {/* message login successful or failed */}
 <div className="flex justify-center item-center"> 
     {message.message && <Message message={message.message} type={message.type} />}
 </div>
