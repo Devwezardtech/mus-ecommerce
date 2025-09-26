80979ff083dfed83005ecac6c29320cb53051883
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-09-26 10:43:38 +0800

    fix email

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,14 +6,15 @@ const router = express.Router();
 
 // Nodemailer setup
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: Number(process.env.EMAIL_PORT) === 465, 
+  host: "smtp.gmail.com", 
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 
 
 // Signup with OTP
