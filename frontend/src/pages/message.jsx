b649686768fef0a954508ff27cc491a70c83fa02
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-09-26 16:35:41 +0800

    fix: message fixed

--- frontend/src/pages/message.jsx
+++ frontend/src/pages/message.jsx
@@ -3,19 +3,19 @@ const Message = ({ message, type }) => {
   let textColor = "";
 
   if (type === "success") {
-    bgColor = "bg-green-400";
+    bgColor = "bg-green-500";
     textColor = "text-white";
   } else if (type === "loading") {
     bgColor = "bg-white";
     textColor = "text-black";
   } else {
-    bgColor = "bg-red-200";
+    bgColor = "bg-red-500";
     textColor = "text-white";
   }
 
   return (
     <div
-      className={`fixed top-20 z-50 ${bgColor} ${textColor} px-4 py-2 rounded shadow-lg backdrop-blur-md animate-fade`}
+      className={`fixed top-20 z-50 ${bgColor} ${textColor} px-4 py-2 rounded-sm  animate-fade`}
     >
       {message}
     </div>
