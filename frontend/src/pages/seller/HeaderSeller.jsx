57e45b66d1a652233014fc1c27b8de157ab506ee
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-10-27 01:20:53 +0800

    fix: the logout condition has issue and fixed

--- frontend/src/pages/seller/HeaderSeller.jsx
+++ frontend/src/pages/seller/HeaderSeller.jsx
@@ -109,7 +109,7 @@ const HeaderSeller = ({ onAddProduct }) => {
       {showLogout && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
           <div className="bg-white p-8 rounded-lg shadow-lg">
-            <p className="mb-4 text-lg">Are you sure you want to logout?</p>
+            <p className="mb-4 text-lg text-black">Are you sure you want to logout?</p>
             <div className="flex justify-end gap-2">
               <button onClick={cancelLogout} className="px-3 py-2 bg-blue-500 text-white hover:bg-blue-400 rounded-lg">Cancel</button>
               <button onClick={confirmLogout} className="px-6 py-2 bg-red-500 hover:bg-red-400 text-white rounded-lg">Ok</button>
