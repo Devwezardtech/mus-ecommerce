f2aef82f7252b84cec696713760ebd3e5411308b
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-02 03:47:41 +0800

    inhance: some feature

--- frontend/src/pages/seller/page/page_monthly.jsx
+++ frontend/src/pages/seller/page/page_monthly.jsx
@@ -3,7 +3,14 @@ import SalesChart from "../../../components/chart/SalesChart";
 const PagesMonthly = () => {
 return(
 <div>
+    <div className="flex justify-center gap-14 px-4" >
+      <div>
+         <PageNav />
+      </div>
+      <div>
    <SalesChart />
+   </div>
+   </div>
 </div>
 )
 }
