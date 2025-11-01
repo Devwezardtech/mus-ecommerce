2bc71a2d42e144aec67c1b8303610eb90d42e3c0
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-02 03:43:11 +0800

    inhance: last commit

--- frontend/src/pages/seller/page/page_today.jsx
+++ frontend/src/pages/seller/page/page_today.jsx
@@ -1,11 +1,14 @@
 import TodayRevenueChart from "../../../components/chart/TodayRevenueChart";
 
+
 const PagesToday = () => {
    return(
 <div>
    <TodayRevenueChart />
 </div>
    );
+
 }
+
 
 export default PagesToday;
