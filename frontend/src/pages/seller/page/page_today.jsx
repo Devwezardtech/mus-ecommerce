c2a7d1dd5501e7ead2588eaaae0b64d45810de10
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-02 03:45:06 +0800

    fixed

--- frontend/src/pages/seller/page/page_today.jsx
+++ frontend/src/pages/seller/page/page_today.jsx
@@ -1,10 +1,18 @@
 import TodayRevenueChart from "../../../components/chart/TodayRevenueChart";
 
 
+
 const PagesToday = () => {
    return(
 <div>
+   <div className="flex justify-center gap-14 px-4" >
+      <div>
+         <PageNav />
+      </div>
+      <div>
    <TodayRevenueChart />
+   </div>
+   </div>
 </div>
    );
 
