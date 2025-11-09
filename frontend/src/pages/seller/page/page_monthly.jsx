4ff91f7c01833e4a343e0bf146b760c42a979ffb
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-10 00:26:11 +0800

    inhance: add some features

--- frontend/src/pages/seller/page/page_monthly.jsx
+++ frontend/src/pages/seller/page/page_monthly.jsx
@@ -1,9 +1,11 @@
 import SalesChart from "../../../components/chart/SalesChart";
+import HeaderSeller from "../HeaderSeller";
 import PageNav from "../page_nav/page_nave";
 
 const PagesMonthly = () => {
 return(
 <div>
+   <HeaderSeller />
     <div className=" pt-24 flex justify-center gap-14 px-4" >
       <div>
          <PageNav />
--- frontend/src/pages/seller/page/page_today.jsx
+++ frontend/src/pages/seller/page/page_today.jsx
@@ -1,10 +1,12 @@
 import TodayRevenueChart from "../../../components/chart/TodayRevenueChart";
+import HeaderSeller from "../HeaderSeller";
 import PageNav from "../page_nav/page_nave";
 
 
 const PagesToday = () => {
    return(
 <div>
+   <HeaderSeller />
    <div className="flex justify-center gap-14 px-4" >
       <div>
          <PageNav />
