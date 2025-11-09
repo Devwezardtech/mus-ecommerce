90baf2baa421ba735c8ff4060aff841d47a883fc
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-10 01:25:17 +0800

    inhance

--- frontend/src/pages/seller/SellerStrats.jsx
+++ frontend/src/pages/seller/SellerStrats.jsx
@@ -8,10 +8,7 @@ import TodayRevenueChart from "../../components/chart/TodayRevenueChart";
 const SellerStrats = () => {
 return (
    <div>
-      <div className="fixed w-full z-50">
-        <HeaderSeller />
-      </div>
-      <div className="pt-16 px-4 sm:px-8 md:px-16 lg:px-18 sm:pt-20 md:pt-24 lg:pt-28">
+      <div className=" pt-16 px-4 sm:px-8 md:px-16 lg:px-18 sm:pt-20 md:pt-24 lg:pt-28">
          <div className="mb-8">
             <TodayRevenueChart />
          </div>
--- frontend/src/pages/seller/page/page_weekly.jsx
+++ frontend/src/pages/seller/page/page_weekly.jsx
@@ -1,12 +1,16 @@
 import WeeklyLineChart from "../../../components/chart/WeeklyLineChart";
 import HeaderSeller from "../HeaderSeller";
 import PageNav from "../page_nav/page_nave";
+import SellerStrats from "../SellerStrats";
 
 const PagesWeekly = () => {
    return(
 <div>
    <HeaderSeller />
-   <div className=" pt-4 flex justify-center gap-14 px-4" >
+   <div className="flex md:hidden">
+      <SellerStrats />
+   </div>
+   <div className="hidden md:flex pt-4 flex justify-center gap-14 px-4" >
       <div>
          <PageNav />
       </div>
