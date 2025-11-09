1856915823cea8ae68bee10f843c402614a2dcd3
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-10 00:27:58 +0800

    inhance: adding header each strat

--- frontend/src/pages/seller/page/page_weekly.jsx
+++ frontend/src/pages/seller/page/page_weekly.jsx
@@ -1,9 +1,11 @@
 import WeeklyLineChart from "../../../components/chart/WeeklyLineChart";
+import HeaderSeller from "../HeaderSeller";
 import PageNav from "../page_nav/page_nave";
 
 const PagesWeekly = () => {
    return(
 <div>
+   <HeaderSeller />
    <div className="flex justify-center gap-14 px-4" >
       <div>
          <PageNav />
