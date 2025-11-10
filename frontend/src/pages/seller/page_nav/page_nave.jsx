08a29fd6524afd78938df50a265bf2568e77871b
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-11 00:44:34 +0800

    inhance and fix

--- frontend/src/pages/seller/page_nav/page_nave.jsx
+++ frontend/src/pages/seller/page_nav/page_nave.jsx
@@ -7,7 +7,7 @@ const PageNav = () => {
     <div className="">
       {/* Fixed header */}
       <nav className="flex justify-start w-full  h-100%">
-         <div className="flex flex-col w-30 px-2 gap-2 w-full font-semibold">
+         <div className="flex flex-col w-30 px-4 gap-2 w-full font-semibold">
           <Link className="hover:bg-white" to="/saletoday">Today</Link>
         <Link className="hover:bg-white" to="/saleweekly">Weekly</Link>
         <Link className="hover:bg-white" to="/salemonthly">Monthly</Link>
