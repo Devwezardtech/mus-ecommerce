ca39f65022d656e58b5ce4500174c25e97f6ee7f
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-12 00:53:48 +0800

    2

--- frontend/src/pages/seller/SellerProfilemain.jsx
+++ frontend/src/pages/seller/SellerProfilemain.jsx
@@ -68,6 +68,7 @@ const SellerProfileMain = () => {
           {/* Edit Profile Button */}
           <div className="text-right">
             <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
+              
               Edit Profile
             </button>
           </div>
