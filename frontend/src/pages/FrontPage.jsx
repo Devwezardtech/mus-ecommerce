fc9964d442c52e4f4a4c0ac03dc236e88d0d1d47
Devwezardtech <ejemarmaloloyon007@gmail.com>
2025-11-18 00:30:24 +0800

    enhance: add some the same code about frontpage

--- frontend/src/pages/FrontPage.jsx
+++ frontend/src/pages/FrontPage.jsx
@@ -7,6 +7,8 @@ import Login from "./Login";
 import Signup from "./Signup";  
 import DeliverysignUp from "./DeliveryAcc/DeliverySignup";
 import DeliveryLogin from "./DeliveryAcc/deliverlogin";
+import { useNavigate } from "react-router-dom";
+import FrontDisplay from "./Product_category/front_product";
 
 
 
@@ -21,6 +23,8 @@ const FrontPage = () => {
   const [showSignupModal, setModalSignup] = useState(false);
   const [showSignupModalDeliver, setModalSignupDeliver] = useState(false);
   const [showLoginModalDeliver, setModalLoginDeliver] = useState(false);
+
+  const navigate = useNavigate();
   
 
   const [email, setEmail] = useState("");
@@ -36,15 +40,24 @@ const FrontPage = () => {
   }, []);
 
   const fetchProducts = async () => {
+
     try {
-      const res = await api.get("/api/products");
-      setProducts(res.data);
+      // Use timestamp to prevent caching
+      const timestamp = Date.now();
+      const res = await api.get(`/api/products?random=true&limit=40&_=${timestamp}`, {
+        headers: { "Cache-Control": "no-cache" },
+      });
+
+      if (Array.isArray(res.data)) {
+        setProducts(res.data); // replace products with new random set
+      } else {
+        console.error("Expected array but received:", res.data);
+      }
     } catch (error) {
-      const errMsg = error.response?.data?.message || "loading...";
-      console.error(errMsg);
-      showMessage(errMsg, "failed");
+      console.error("Failed to fetch products:", error.message);
     }
   };
+
 
   const handleAddToCart = () => {
     setModalProduct(null)
@@ -69,50 +82,53 @@ const FrontPage = () => {
         />
       </div>
 
-      <div className="p-4 bg-gray-100">
-        {products.length === 0 ? (
-          <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mt-5 gap-4 py-8 lg:mt-16">
-            {Array.from({ length: 32 }).map((_, index) => (
-              <div
-                key={index}
-                className="bg-gray-200 rounded shadow animate-pulse p-1 space-y-1 lg:space-y-4 lg:p-4"
-              >
-                <div className="h-28 bg-gray-300 rounded lg:h-48"></div>
-                <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-                <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-                <div className="h-4 bg-gray-300 rounded w-1/2"></div>
-                <div className="flex justify-between mx-1 gap-1 lg:mt-2 ">
-                  <div className="h-5 w-20 bg-gray-300 lg:h-8 rounded"></div>
-                  <div className="h-5 w-20  w-20 bg-gray-300 lg:h-8 rounded"></div>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div className="pt-10 sm:pt-14 md:pt-16 lg:pt-18">
-
-            <div className="grid gap-4 px-1 py-4 grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-              {products.map((product) => (
-                <div key={product._id}>
-                  <div className="bg-gray-250 mb-4 pb-2 flex flex-col rounded shadow-sm hover:shadow-md transition-shadow duration-300 gap-2 w-auto h-auto">
-                    <button onClick={() => setModalProduct(product)}>
-                      <div className="flex flex-col items-center justify-center">
-                        <img
-               src={product.photo}
-               alt={product.name}
-                          className="h-28 w-28 object-cover rounded shadow sm:h-32 sm:w-32 md:h-40 md:w-40 lg:h-60 lg:w-60 "/>
-                        <div>
-                          <strong className="text-gray-800 text-sm font-semibold sm:text-base md:text-md lg:text-lg">{product.name}</strong>
-                          <p className="text-xs">{product.description}</p>
-                          <p>
-                            <strong className="text-sm font-semibold sm:text-base md:text-md lg:text-lg">₱ {product.price}</strong>
-                          </p>
-                          <p className="text-sx text-sm text-gray-500 sm:text-sm md:text-sm lg:text-md">
-                            Stock: {product.stock}
-                          </p>
+       <div className="p-4">
+             {products.length === 0 ? (
+               <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 mt-5 gap-4 py-8 lg:mt-16">
+                 {Array.from({ length: 32 }).map((_, index) => (
+                   <div key={index} className="bg-gray-200 rounded shadow animate-pulse p-1 space-y-1 lg:space-y-4 lg:p-4">
+                     <div className="h-28 bg-gray-300 rounded lg:h-48"></div>
+                     <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+                     <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+                     <div className="h-4 bg-gray-300 rounded w-1/2"></div>
+                     <div className="flex justify-between mx-1 gap-1 lg:mt-2 ">
+                       <div className="h-5 w-20 bg-gray-300 lg:h-8 rounded"></div>
+                       <div className="h-5 w-20 bg-gray-300 lg:h-8 rounded"></div>
+                     </div>
+                   </div>
+                 ))}
+               </div>
+             ) : (
+               <div>
+                 <div className="w-full pt-8">
+                   <FrontDisplay />
+                 </div>
+     
+                 <div className="pt-10 sm:pt-14 md:pt-16 lg:pt-18">
+                   <h2 className="font-semibold">All Products</h2>
+                   <div className="grid gap-4 px-1 py-4 grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+                     {products.map((product) => (
+                       <div key={product._id}>
+                         <div className="bg-gray-250 mb-4 pb-2 flex flex-col rounded shadow-md hover:shadow-lg transition-shadow duration-300 gap-2 w-auto h-auto">
+                    <button onClick={() => navigate(`/product/${product._id}`)}>
+                        <div className="flex flex-col items-center justify-center">
+                          <img
+                            src={Array.isArray(product.photo) ? product.photo[0] : product.photo}
+                            alt={product.name}
+                            className="h-28 w-28 object-cover rounded shadow sm:h-32 sm:w-32 md:h-40 md:w-40 lg:h-60 lg:w-60"
+                          />
+                          <div>
+                            <strong className="text-gray-800 text-sm font-semibold sm:text-base md:text-md lg:text-lg">{product.name}</strong>
+                            <p className="text-xs">{product.description}</p>
+                            <p>
+                              <strong className="text-sm font-semibold sm:text-base md:text-md lg:text-lg">${product.price}</strong>
+                            </p>
+                            <p className="text-sx text-sm text-gray-500 sm:text-sm md:text-sm lg:text-md">
+                              Stock: {product.stock}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </button>
+                      </button>
 
                     <div className="flex items-center justify-center gap-1">
                       <button
@@ -133,6 +149,7 @@ const FrontPage = () => {
                 </div>
               ))}
             </div>
+          </div>
           </div>
         )}
       </div>
